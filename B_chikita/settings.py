dfb5940aa8efe50f30ad5b1cd5393f2f00aeaef4
vanderlei <junior_12morais@hotmail.com>
2023-11-04 23:52:15 -0400

    modified:   B_chikita/__pycache__/settings.cpython-311.pyc 	modified:   B_chikita/settings.py

--- B_chikita/settings.py
+++ B_chikita/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'B_chikita.urls'
@@ -116,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
